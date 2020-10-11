3d2d4933fe9a184853c1a66debc6afe131c0f59d
SteelBoshido99 <SteelBoshido99@users.noreply.github.com>
2020-10-11 18:58:15 +0100

    COMP200-1b
    Completed Inheritance

--- src/com/Main.java
+++ src/com/Main.java
@@ -4,20 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Truck truck = new Truck();
-        truck.setNumWheels(8);
-        truck.setSpeed(60.5f);
-        truck.drive();
-
-        System.out.println(truck.getNumWheels());
-        System.out.println(truck.getSpeed());
-
-        Suitcase suitcase = new Suitcase(7);
-        suitcase.addItem();
-        suitcase.addItem();
-        suitcase.addItem();
-        suitcase.removeItem();
-        suitcase.removeItem();
+    Car mustang = new Car();
+    mustang.move();
 
     }
 
---
+++ src/com/Vehicle.java
@@ -0,0 +1,13 @@
+package com;
+
+
+public class Vehicle {
+    public int numPassengers;
+
+    public void move() {
+//begin of modifiable zone(JavaCode)......C/899c1331-1e9a-4adb-bb1b-2bfb0add6234
+    System.out.println("Using vehicle");
+//end of modifiable zone(JavaCode)........E/899c1331-1e9a-4adb-bb1b-2bfb0add6234
+    }
+
+}
